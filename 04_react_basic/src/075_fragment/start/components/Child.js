91f8e2b1a2b209f3750ae1aa3c99d9adaddd8171
manaki-e <48690114s@gmail.com>
2023-02-01 14:47:02 +0900

    075_fragment

--- 04_react_basic/src/075_fragment/start/components/Child.js
+++ 04_react_basic/src/075_fragment/start/components/Child.js
@@ -1,11 +1,21 @@
 import "./Child.css";
+import React from "react";
 
 const Child = () => {
-  return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
-  );
+	return (
+		<React.Fragment>
+			<div className="component">
+				<h3>Hello Component</h3>
+			</div>
+			<h3>Hello Flagment</h3>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Aspernatur, debitis eius nesciunt eligendi, vero delectus
+				voluptatem, porro similique eveniet minus non obcaecati illo
+				fuga excepturi velit iure ea tempore quo?
+			</p>
+		</React.Fragment>
+	);
 };
 
 export default Child;
