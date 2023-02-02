521e61c9e83703ff6d754c9e3ae8ea829e12086c
manaki-e <48690114s@gmail.com>
2023-02-02 15:50:24 +0900

    050_prev_state

--- 05_state_and_event/src/050_prev_state/start/Example.js
+++ 05_state_and_event/src/050_prev_state/start/Example.js
@@ -1,7 +1,24 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+	let [default_value, count] = useState(0);
+	const countUp = () => {
+		count(default_value + 1);
+	};
+	const countDown = () => {
+		count(default_value - 1);
+	};
+	return (
+		<>
+			<p>現在のカウント数：{default_value}</p>
+			<button className="border py-2 py-4" onClick={countUp}>
+				+
+			</button>
+			<button className="border py-2 py-4" onClick={countDown}>
+				+
+			</button>
+		</>
+	);
 };
 
 export default Example;
