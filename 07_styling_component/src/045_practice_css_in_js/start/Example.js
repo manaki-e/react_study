6b0cd524840e879dcc423ae1f1b93b36fa570736
manaki-e <48690114s@gmail.com>
2023-02-03 14:42:50 +0900

    045_practice_css_in_js

--- 07_styling_component/src/045_practice_css_in_js/start/Example.js
+++ 07_styling_component/src/045_practice_css_in_js/start/Example.js
@@ -1,35 +1,24 @@
 import styled from "styled-components";
 
 const FirstButton = styled.button`
-  display: inline-block;
-  border-radius: 3px;
-  padding: 0.5rem 0;
-  margin: 0.5rem 1rem;
-  width: 11rem;
-  border: none;
+	display: inline-block;
+	border-radius: 3px;
+	padding: 0.5rem 0;
+	margin: 0.5rem 1rem;
+	width: 11rem;
+	border: none;
 `;
 
 const Example = () => {
-  return (
-    <>
-      <h3>練習問題</h3>
-      <p>
-        記述を変更し、完成コードと同じ状態になるようにしてください。
-        <ul>
-          <li>Q1. FirstButtonのbackgroudをpinkにしてください。</li>
-          <li>
-            Q2.
-            FirstButtonを継承したSecondButtonを作成し、backgroudをredに、colorをwhiteにしてください。{" "}
-          </li>
-          <li>
-            Q3.
-            SecondButtonを継承したThirdButtonを作成し、props.darkがある場合のみbackgroudがblackに、ない場合はgreenになるようにしてください。
-          </li>
-        </ul>
-      </p>
-      <FirstButton>ボタン1</FirstButton>
-    </>
-  );
+	return (
+		<>
+			<h3>練習問題</h3>
+			<FirstButton className="bg-pink-200">ボタン1</FirstButton>
+			<FirstButton className="bg-red-400 text-white">ボタン2</FirstButton>
+			<FirstButton className="bg-green-600 text-white">ボタン3</FirstButton>
+			<FirstButton data-color="dark" className="bg-green-600 text-white data-[color=dark]:bg-black" >ボタン4</FirstButton>
+		</>
+	);
 };
 
 export default Example;
